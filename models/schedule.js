df17ecce8ee5ee3e91ad2f8d16188bce027d78dd
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-05 02:23:08 +0000

    schedule モデルを追加

---
+++ models/schedule.js
@@ -0,0 +1,34 @@
+'use strict';
+const loader = require('./sequelize-loader');
+const Sequelize = loader.Sequelize;
+
+const Schedule = loader.database.define('schedules',
+  {
+    scheduleId: {
+      type: Sequelize.UUID,
+      primaryKey: true,
+      allowNull: false
+    },
+    scheduleName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    memo: {
+      type: Sequelize.TEXT,
+      allowNull: false
+    },
+    createdBy: {
+      type: Sequelize.DATE,
+      allowNull: false
+    }
+  }, {
+    freezeTableName: true,
+    timestamps: false,
+    indexes: [
+      {
+        fields: ['createdBy']
+      }
+    ]
+  });
+
+module.exports = Schedule;
