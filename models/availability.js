bd83470d05e95af8ae0d51960b56cd81a6d3ec7c
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-05 02:47:51 +0000

    Availabilityモデルを追加

---
+++ models/availability.js
@@ -0,0 +1,32 @@
+'use strict';
+const loader = require('./sequelize-loader');
+const Sequelize = loader.Sequelize;
+
+const Availability = loader.database.define('availabilities',
+  {
+    candidateId: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      allowNull: false
+    },
+    userId: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      allowNull: false
+    },
+    availability: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    }
+  }, {
+    freezeTableName: true,
+    timestamps: false,
+    indexes: [
+      {
+        fields: ['scheduleId']
+      }
+    ]
+  });
+
+module.exports = Availability;
